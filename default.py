3ad1b18b6a3673420c3c4f99b59abac7a4f77375
edit4ever <edit4ever@hotmail.com>
2017-07-07 10:41:54 -0700

    fix login error and add clear cache option

--- default.py
+++ default.py
@@ -17,8 +17,20 @@
 #  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
-import xbmcgui
+import xbmc,xbmcaddon,xbmcgui
+import os
 
 dialog = xbmcgui.Dialog()
-if dialog.yesno('This is addon runs from within Tvheadend', 'Add your zap2it/screener/tvguide login info in the addon configuration.  Reboot and setup the grabber in tvheadend before enabling the other options.'):
-    xbmc.executebuiltin(Addon.OpenSettings())
+if dialog.yesno('This is addon runs from within Tvheadend', 'Add your zap2it/screener/tvguide login info in the addon configuration.  Then reboot and setup the grabber channels in tvheadend before enabling the other options.', 'Would you like to open the addon settings?'):
+    xbmcaddon.Addon().openSettings()
+if dialog.yesno('Clear the listings cache', 'Would you like to clear the listings cache?', 'This might be needed to download updated "favorite" channels.'):
+    scandirs = xbmc.translatePath(
+    'special://home/addons/script.module.zap2xml/cache'
+    )
+    path = scandirs
+    exts = ('html.gz')
+    if os.path.exists(scandirs):
+        for root, dirs, files in os.walk(path):
+            for currentFile in files:
+                if any(currentFile.lower().endswith(exts) for ext in exts):
+                    os.remove(os.path.join(root, currentFile))
